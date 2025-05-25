7fdd14023d810fe0e8627c5bcadb2f3023884395
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-25 10:54:12 +0530

    Add files via upload

---
+++ app83.js
@@ -0,0 +1,13 @@
+console.log("Hi there!");
+
+let id = setInterval( () => {
+    console.log("Apna College");
+}, 2000);
+
+console.log(id);
+
+let id2 = setInterval( () => {
+    console.log("Shreyas Pagar");
+}, 3000);
+
+console.log(id2);
